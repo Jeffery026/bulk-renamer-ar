4968a9372b98557f66be5759e4339457d222609e
Jeffery026 <Yok5060yok@gmail.com>
2026-09-19 23:09:37 +0300

    refactor: files_provider.dart

--- lib/providers/files_provider.dart
+++ lib/providers/files_provider.dart
@@ -6,145 +6,117 @@ enum SortBy { name, date, size, extension }
 enum SortOrder { ascending, descending }
 
 class FileItem {
-  final File file;
+  final FileSystemEntity entity;
   bool selected;
-  FileItem({required this.file, this.selected = false});
-  String get name => file.path.split('/').last;
-  String get extension {
+  FileItem({required this.entity, this.selected = false});
+  bool get isDir => entity is Directory;
+  bool get isFile => entity is File;
+  String get name => entity.path.split('/').last;
+  File get file => entity as File;
+  String get ext {
+    if (isDir) return '';
     final dot = name.lastIndexOf('.');
     return dot > 0 ? name.substring(dot + 1).toLowerCase() : '';
   }
 }
 
 class FilesProvider extends ChangeNotifier {
-  List<FileItem> _items = [];
+  List<FileItem> _all = [];
   String _currentPath = '/storage/emulated/0';
-  List<String> _pathHistory = [];
+  final List<String> _history = [];
   SortBy _sortBy = SortBy.name;
   SortOrder _sortOrder = SortOrder.ascending;
-  String _searchQuery = '';
+  String _search = '';
   bool _showHidden = false;
   bool _permissionGranted = false;
   String? _outputPath;
-  bool _deleteOriginal = false;
+  bool _deleteOriginal = true;
 
-  List<FileItem> get items => _filteredAndSorted();
-  List<FileItem> get selectedItems => _items.where((f) => f.selected).toList();
-  int get selectedCount => _items.where((f) => f.selected).length;
-  String get currentPath => _currentPath;
-  List<String> get pathHistory => _pathHistory;
-  SortBy get sortBy => _sortBy;
-  SortOrder get sortOrder => _sortOrder;
-  String get searchQuery => _searchQuery;
-  bool get showHidden => _showHidden;
   bool get permissionGranted => _permissionGranted;
+  bool get canGoBack => _history.isNotEmpty;
+  String get currentPath => _currentPath;
   String? get outputPath => _outputPath;
   bool get deleteOriginal => _deleteOriginal;
-  bool get canGoBack => _pathHistory.isNotEmpty;
+  bool get showHidden => _showHidden;
+  SortOrder get sortOrder => _sortOrder;
 
-  List<FileItem> _filteredAndSorted() {
-    var list = _items.where((f) {
+  List<FileItem> get items {
+    var list = _all.where((f) {
       if (!_showHidden && f.name.startsWith('.')) return false;
-      if (_searchQuery.isNotEmpty && !f.name.toLowerCase().contains(_searchQuery.toLowerCase())) return false;
+      if (_search.isNotEmpty && !f.name.toLowerCase().contains(_search.toLowerCase())) return false;
       return true;
     }).toList();
-
-    list.sort((a, b) {
-      int cmp;
+    final dirs = list.where((f) => f.isDir).toList()
+      ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+    var files = list.where((f) => f.isFile).toList();
+    files.sort((a, b) {
+      int c;
       switch (_sortBy) {
-        case SortBy.name: cmp = a.name.toLowerCase().compareTo(b.name.toLowerCase()); break;
-        case SortBy.date: cmp = a.file.statSync().modified.compareTo(b.file.statSync().modified); break;
-        case SortBy.size: cmp = a.file.lengthSync().compareTo(b.file.lengthSync()); break;
-        case SortBy.extension: cmp = a.extension.compareTo(b.extension); break;
+        case SortBy.name: c = a.name.toLowerCase().compareTo(b.name.toLowerCase()); break;
+        case SortBy.date: c = a.file.statSync().modified.compareTo(b.file.statSync().modified); break;
+        case SortBy.size: c = a.file.lengthSync().compareTo(b.file.lengthSync()); break;
+        case SortBy.extension: c = a.ext.compareTo(b.ext); break;
       }
-      return _sortOrder == SortOrder.ascending ? cmp : -cmp;
+      return _sortOrder == SortOrder.ascending ? c : -c;
     });
-    return list;
+    return [...dirs, ...files];
   }
+
+  List<FileItem> get selectedItems => _all.where((f) => f.isFile && f.selected).toList();
+  int get selectedCount => selectedItems.length;
 
   Future<bool> requestPermission() async {
     if (Platform.isAndroid) {
-      final status = await Permission.manageExternalStorage.request();
-      if (!status.isGranted) {
-        final legacy = await Permission.storage.request();
-        _permissionGranted = legacy.isGranted;
-      } else {
-        _permissionGranted = true;
-      }
-    } else {
-      _permissionGranted = true;
-    }
+      var s = await Permission.manageExternalStorage.request();
+      if (!s.isGranted) s = await Permission.storage.request();
+      _permissionGranted = s.isGranted;
+    } else { _permissionGranted = true; }
     notifyListeners();
     return _permissionGranted;
   }
 
   Future<void> loadDirectory(String path) async {
-    if (!_permissionGranted) await requestPermission();
     try {
       final dir = Directory(path);
       if (!dir.existsSync()) return;
-      final entries = dir.listSync();
-      _items = entries
-          .whereType<File>()
-          .map((f) => FileItem(file: f))
-          .toList();
+      _all = dir.listSync().map((e) => FileItem(entity: e)).toList();
       _currentPath = path;
       notifyListeners();
     } catch (_) {}
   }
 
-  Future<List<Directory>> getSubDirectories(String path) async {
-    try {
-      return Directory(path).listSync().whereType<Directory>().toList();
-    } catch (_) { return []; }
-  }
-
-  void navigateTo(String path) {
-    _pathHistory.add(_currentPath);
-    loadDirectory(path);
-  }
-
-  void navigateBack() {
-    if (_pathHistory.isNotEmpty) {
-      loadDirectory(_pathHistory.removeLast());
-      notifyListeners();
-    }
-  }
-
-  void toggleSelect(FileItem item) {
-    item.selected = !item.selected;
-    notifyListeners();
-  }
-
-  void selectAll() {
-    for (final f in _items) { f.selected = true; }
-    notifyListeners();
-  }
-
-  void clearSelection() {
-    for (final f in _items) { f.selected = false; }
-    notifyListeners();
-  }
-
-  void setSort(SortBy by, SortOrder order) {
-    _sortBy = by; _sortOrder = order; notifyListeners();
-  }
-
-  void setSearch(String q) { _searchQuery = q; notifyListeners(); }
+  void navigateTo(String path) { _history.add(_currentPath); loadDirectory(path); }
+  void navigateBack() { if (_history.isNotEmpty) loadDirectory(_history.removeLast()); }
+  void toggleSelect(FileItem item) { if (item.isFile) { item.selected = !item.selected; notifyListeners(); } }
+  void selectAll() { for (final f in _all) { if (f.isFile) f.selected = true; } notifyListeners(); }
+  void clearSelection() { for (final f in _all) { f.selected = false; } notifyListeners(); }
+  void setSort(SortBy by, SortOrder order) { _sortBy = by; _sortOrder = order; notifyListeners(); }
+  void setSearch(String q) { _search = q; notifyListeners(); }
   void toggleHidden() { _showHidden = !_showHidden; notifyListeners(); }
-  void setOutputPath(String? path) { _outputPath = path; notifyListeners(); }
+  void setOutputPath(String? p) { _outputPath = p; notifyListeners(); }
   void setDeleteOriginal(bool v) { _deleteOriginal = v; notifyListeners(); }
 
   List<Directory> getStorageRoots() {
-    final roots = <Directory>[];
-    final internal = Directory('/storage/emulated/0');
-    if (internal.existsSync()) roots.add(internal);
-    final sd = Directory('/storage');
-    if (sd.existsSync()) {
-      for (final d in sd.listSync().whereType<Directory>()) {
-        if (d.path != '/storage/emulated' && d.path != '/storage/self') roots.add(d);
+    final r = <Directory>[];
+    final i = Directory('/storage/emulated/0');
+    if (i.existsSync()) r.add(i);
+    try {
+      for (final d in Directory('/storage').listSync().whereType<Directory>()) {
+        if (!d.path.contains('emulated') && d.path != '/storage/self') r.add(d);
       }
-    }
-    return roots;
+    } catch (_) {}
+    return r;
+  }
+
+  List<String> get breadcrumbs {
+    final rel = _currentPath.replaceFirst(RegExp(r'/storage/emulated/0/?'), '');
+    if (rel.isEmpty) return ['التخزين الداخلي'];
+    return ['التخزين الداخلي', ...rel.split('/').where((s) => s.isNotEmpty)];
+  }
+
+  void navigateToBreadcrumb(int i) {
+    if (i == 0) { navigateTo('/storage/emulated/0'); return; }
+    final crumbs = breadcrumbs;
+    navigateTo('/storage/emulated/0/' + crumbs.sublist(1, i + 1).join('/'));
   }
 }
