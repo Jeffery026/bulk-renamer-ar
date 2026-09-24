450776d5ac3b5fd7c251bc23ae39dfa081817a88
Jeffery026 <Yok5060yok@gmail.com>
2026-09-25 00:07:33 +0300

    v4: main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,17 @@ import 'package:provider/provider.dart';
 import 'theme/app_theme.dart';
 import 'providers/files_provider.dart';
 import 'providers/operations_provider.dart';
+import 'providers/settings_provider.dart';
 import 'screens/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarDividerColor: Colors.transparent,
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const BulkRenamerApp());
 }
 
@@ -22,21 +28,24 @@ class BulkRenamerApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => FilesProvider()),
         ChangeNotifierProvider(create: (_) => OperationsProvider()),
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
       ],
-      child: MaterialApp(
-        title: 'مُعيد التسمية',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.system,
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [Locale('ar'), Locale('en')],
-        locale: const Locale('ar'),
-        home: const HomeScreen(),
+      child: Consumer<SettingsProvider>(
+        builder: (_, settings, __) => MaterialApp(
+          title: 'مُعيد التسمية',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme(settings.fontSize),
+          darkTheme: AppTheme.darkTheme(settings.fontSize),
+          themeMode: settings.themeMode,
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [Locale('ar'), Locale('en')],
+          locale: const Locale('ar'),
+          home: const HomeScreen(),
+        ),
       ),
     );
   }
